6299eb6cfc32388fd030ece0694a5447f6f376c6
artemetra <artemlukin15@gmail.com>
2021-12-15 01:55:06 +0200

    145 solution

---
+++ 145. ASCII Digital clock/main.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from strs import asciiset
+
+def ascii_time(timestr: str) -> str:
+    # somehow, this solution seems to be more concise that for loops,
+    # that were my initial idea. what all of this does is basically
+    # just loop over all the characters and render them line by line.
+    # i might rewrite this to be more readable and scalable (removing
+    # constants like 5), but for now here it is.
+    return "\n".join(
+        [
+            "   ".join([asciiset[ch][1:-1].split('\n')[line] for ch in timestr])
+            for line in range(5)
+        ]
+    )
+
+if __name__ == '__main__':
+    now = datetime.now().strftime("%H:%M:%S")
+    ascii_time(now)
